c2456b1abb6dcc7c9008d5aaec0358c232ae5b7d
Matthew Machuga <machuga@gmail.com>
2015-06-19 15:24:55 -0400

    Adding seet data for user and meetup
    User data
    Email: user@example.com
    Password: usertest

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+# Standard user for dev environment
+
+User.create!(email: 'user@example.com',
+             password: 'usertest',
+             password_confirmation: 'usertest',
+             confirmed_at: Time.now)
+
+Meetup.create!(name: 'Fake Meetup First',
+               details: 'This meetup is going to be freakin sweet!',
+               starts_at: '2015-06-30 11:00:00',
+               ends_at: '2015-06-30 14:00:00')
