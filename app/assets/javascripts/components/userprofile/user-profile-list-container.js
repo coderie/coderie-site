dda15a7c74b941cf0298e542fb2522d14805168c
Bonk <brandon@usnetting.com>
2015-07-13 23:30:20 -0400

    Followed steps until end.
    Honestly have no idea how to nav to the profile view to debug it. Just
    committing this before monkeying around more.

---
+++ app/assets/javascripts/components/userprofile/user-profile-list-container.js
@@ -0,0 +1,23 @@
+var React = require('react');
+
+var UserProfileListContainer = React.createClass({
+  render: function(){
+    return(
+    var profiles = this.state.userProfiles.map(function(profile)){
+      return (
+        <div key={profile.name}>{profile.name}</div>
+      );
+    });
+    return (
+      <div>{profiles}</div>
+    );
+    );
+  },
+
+  getInitialState: function(){
+  var userProfileData  document.getElementById('user-profile-list-container').dataset.userProfiles
+  }
+
+});
+
+//This component will handle getting the data and distributing it to the rest of our components. We want to hydrate the data the server has dehydrated to the dom.
---
+++ app/assets/javascripts/entry/user-profile-list-entry.js
@@ -0,0 +1,8 @@
+var React = require('react');
+var UserProfileListContainer = require('../components/userprofile/user-profile-list-container.js');
+
+React.render(
+  <div id="user-profile-list-container">
+
+  </div>
+);
